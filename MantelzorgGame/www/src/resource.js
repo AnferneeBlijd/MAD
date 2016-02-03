53b354ad7b5b7b2468168b258e26cf220c4c37ab
AnferneeBlijd <anfernee.blijd@gmail.com>
2016-02-03 18:08:38 -0300

    build 1.0

---
+++ MantelzorgGame/www/src/resource.js
@@ -0,0 +1,10 @@
+var asset = {
+    helloBG_jpg : "asset/helloBG.jpg",
+    start_n_png : "asset/start_n.png",
+    start_s_png : "asset/start_s.png",
+};
+
+var g_resources = [];
+for (var i in asset) {
+    g_resources.push(asset[i]);
+}
